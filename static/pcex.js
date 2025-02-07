3d382b79873b696b353b117b07b7a674dccb3be6
Mohammad Hassany <114956341+moh70pitt@users.noreply.github.com>
2025-02-07 13:55:32 -0500

    help icon before linenumber
    also bumped up the version number

--- static/pcex.js
+++ static/pcex.js
@@ -273,13 +273,21 @@ var pcex = {
 			$(lineContent).addClass('stepline');
 		}
 
+		const lineNumber = isTileDrop
+			? String("  " + pcex.blankLineNumbers[blankLineIndex]).slice(-2)
+			: String("  " + line.number).slice(-2);
+		var lineNumberSpan = document.createElement('span');
+		// To have a fixed length line number, which is 2 in all cases.
+		$(lineNumberSpan).addClass('linenumber').html(lineNumber);
+		$(lineContent).prepend(lineNumberSpan);
+
+		var helpButton = null;
 		if (includeIndentButtons || line.commentList.length > 0) {
 			lineContent.appendChild(document.createTextNode(indentedCode));
 
 			if (line.commentList.length > 0) {
-				var helpButton = document.createElement('a');
-				$(helpButton).addClass('btn-floating btn-small waves-effect waves-light red')
-					.attr('id', 'help_' + line.id);
+				helpButton = document.createElement('a');
+				$(helpButton).addClass('btn-floating btn-small waves-effect waves-light red').attr('id', 'help_' + line.id);
 
 				var helpIcon = document.createElement('i');
 				helpIcon.setAttribute("class", 'material-icons');
@@ -290,10 +298,11 @@ var pcex = {
 				});
 
 				helpButton.appendChild(helpIcon);
-				lineContent.appendChild(helpButton);
+				lineContent.prepend(helpButton);
 
 				if (pcex.currentGoal.fullyWorkedOut == false) {
 					$(helpButton).hide();
+					lineNumberSpan.style.marginLeft = '21px';
 				}
 			}
 
@@ -312,22 +321,12 @@ var pcex = {
 				}
 			}
 		} else {
-			lineContent.innerHTML = indentedCode;
+			lineContent.append(indentedCode);
+      		if (!helpButton) lineNumberSpan.style.marginLeft = '21px';
 		}
-		// if absent, the post-comment line will be highlighted as comment as well
-		// -->
+		
+		// patch: post-comment line will be highlighted as comment as well
 		lineContent.innerHTML += '\n';
-		// <--
-		var lineNumberSpan = document.createElement('span');
-		var lineNumber;
-		if (isTileDrop) {
-			lineNumber = String("  " + pcex.blankLineNumbers[blankLineIndex]).slice(-2)
-		} else {
-			lineNumber = String("  " + line.number).slice(-2);
-		}
-
-		$(lineNumberSpan).addClass('linenumber').html(lineNumber); //To have a fixed length line number, which is 2 in all cases.
-		$(lineContent).prepend(lineNumberSpan);
 
 		return lineContent;
 	},
@@ -921,7 +920,6 @@ var pcex = {
 			if (pcex.numberOfTrials >= pcex.numberOfTrialsForHint) {
 				$('#show-hint-button').show();
 			}
-
 		}
 	},
 
@@ -1115,7 +1113,9 @@ var pcex = {
 
 
 		$.each(pcex.currentGoal.lineList, function (i, line) {
-			$("#help_" + line.id).unbind('click').click(pcex.handleHelpButtonClicked).show();
+			const helpBtn = $("#help_" + line.id);
+			helpBtn.unbind('click').click(pcex.handleHelpButtonClicked).show();
+      		helpBtn.next().css('margin-left', '0px');
 		});
 
 		pcex.higlightCorrectBlankLines(pcex.blankLineIDs);
